b41d0f4ede9e31aac4baef3981ec8a15bdc7119a
Esat-Aydin <esat61_2001@hotmail.com>
2022-09-28 22:13:04 +0200

    base service and frontend changes

--- Eazie.Web/Program.cs
+++ Eazie.Web/Program.cs
@@ -1,6 +1,18 @@
+using Eazie.Web;
+using Eazie.Web.Services;
+using Eazie.Web.Services.IServices;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHttpClient<IProductService, ProductService>();
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+
+builder.Services.AddScoped<IProductService, ProductService>();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
---
+++ Eazie.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Eazie.Web.Models;
+
+namespace Eazie.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
